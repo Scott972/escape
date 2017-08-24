06f931be79d4fcf237fcfee5d05f0fcb48f621cf
scott972 <emmanuel@fastbraces.com>
2017-08-24 11:52:33 -0500

    wtf is this

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -18,8 +18,11 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTargetLocation().ToString();
+	//wtf
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 
-	// ...
 	
 }
 
